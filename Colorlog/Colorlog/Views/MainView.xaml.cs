1175e6a3b70d516d42a89fdf2ecad5dc6163907f
seongeun0629 <seongeun0629@gmail.com>
2026-04-05 23:22:48 +0900

    feat: 사이드바 슬라이딩 애니메이션 구현 및 메인 메뉴 구성 - 빌드 파일 제외를 위한 .gitignore, 줄바꿈 처리를 위한 .gitattributes 추가 - storyboard를 이용한 사이드바 Open/Close 애니메이션 추가 - 햄버거 버튼 하나로 제어 가능한 토글 로직 구현 - 서비스 성격에 맞춘 5가지 메뉴(홈, 분석, 히스토리, 뷰티템, 설정) 배치 - Styles : Colorlog 전용 테마 색상(PrimaryBrush) 및 카드/버튼 스타일 리소스 정의

---
+++ Colorlog/Colorlog/Views/MainView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Colorlog.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+        bool isSidebarOpen = false;
+        private void BtnToggle_Click(object sender, RoutedEventArgs e)
+        {
+            if (isSidebarOpen)
+            {
+                ((Storyboard)this.FindResource("CloseSidebar")).Begin();
+            }
+            else
+            {
+                ((Storyboard)this.FindResource("OpenSidebar")).Begin();
+            }
+            isSidebarOpen = !isSidebarOpen; 
+        }
+
+        private void exitProgram(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
